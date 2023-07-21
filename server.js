fa16cfb588181ac255ee872b180af7fbd5d6b2cf
yimmisam <111089385+yimmisam@users.noreply.github.com>
2023-07-21 18:04:31 -0500

    subir archivos a server

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const ejs = require('ejs');
 const path = require('path');
-const cdnStorage = 'https://tiendas.agencsi.com/storage'
+const urlcdn = 'https://tiendas.agencsi.com'
+const cdnStorage = urlcdn + '/storage';
+
+
+function generarCodigoAleatorioConTimestamp() {
+
+  const codigoAleatorio = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
+
+  const timestamp = Date.now();
+
+  const codigoFinal = `${codigoAleatorio}${timestamp}`;
+
+  return codigoFinal;
+}
+
+const version = generarCodigoAleatorioConTimestamp();
 
 
 const app = express();
@@ -13,12 +28,20 @@ app.set('views', path.join(__dirname, '/views'));
 app.get('/', async (req, res) => {
   const requestedDomain = 'hoppedidos.com' //req.get('host');
   const html = ReactDOMServer.renderToString(
-    React.createElement('h1', null, '¡Hola desde el servidor! '+requestedDomain)
+    React.createElement('h1', null, '¡Hola desde el servidor! ' + requestedDomain)
   );
   const header = await ejs.renderFile(path.join(__dirname, '/views/header.ejs'));
   const footer = await ejs.renderFile(path.join(__dirname, '/views/footer.ejs'));
 
-  res.render('index.ejs', { header, footer, content: html, title: requestedDomain, cdnstorage:cdnStorage});
+  res.render('index.ejs', {
+    header,
+    footer,
+    content: html,
+    domain: requestedDomain,
+    urldcdn: urlcdn,
+    cdnstorage: cdnStorage,
+    version
+  });
 });
 
 app.get('/productos', async (req, res) => {
@@ -33,7 +56,7 @@ app.get('/productos', async (req, res) => {
   const header = await ejs.renderFile(path.join(__dirname, '/views/header.ejs'));
   const footer = await ejs.renderFile(path.join(__dirname, '/views/footer.ejs'));
 
-  res.render('index.ejs', { header, footer, content: productList , title: requestedDomain});
+  res.render('index.ejs', { header, footer, content: productList, title: requestedDomain });
 });
 
 app.use('/client.js', express.static('client.js'));
