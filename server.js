325acdefafaf5549b32ed4f6b480c02300f96c36
yimmisam <111089385+yimmisam@users.noreply.github.com>
2023-07-21 02:06:26 -0500

    nuevo 1

--- server.js
+++ server.js
@@ -4,31 +4,30 @@ const ReactDOMServer = require('react-dom/server');
 const ejs = require('ejs');
 const path = require('path');
 
+
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-// Ruta para la página de inicio
 app.get('/', async (req, res) => {
+  const requestedDomain = req.get('host');
   const html = ReactDOMServer.renderToString(
-    React.createElement('h1', null, '¡Hola desde el servidor!')
+    React.createElement('h1', null, '¡Hola desde el servidor! '+requestedDomain)
   );
-
   const header = await ejs.renderFile(path.join(__dirname, '/views/header.ejs'));
   const footer = await ejs.renderFile(path.join(__dirname, '/views/footer.ejs'));
 
   res.render('index.ejs', { header, footer, content: html });
 });
 
-// Ruta para la página de productos
 app.get('/productos', async (req, res) => {
   const productos = [
-    { id: 1, nombre: 'Producto 1', precio: 10 },
-    { id: 2, nombre: 'Producto 2', precio: 15 },
-    { id: 3, nombre: 'Producto 3', precio: 20 },
+    { id: 1, nombre: 'Producto ABC 1', precio: 10 },
+    { id: 2, nombre: 'Producto ABC 2', precio: 15 },
+    { id: 3, nombre: 'Producto ABC 3', precio: 20 },
   ];
 
-  const productList = await ejs.renderFile(path.join(__dirname, '/views/modulos/productList.ejs'), { productos });
+  const productList = await ejs.renderFile(path.join(__dirname, '/views/modulos/productos/productList.ejs'), { productos });
   const header = await ejs.renderFile(path.join(__dirname, '/views/header.ejs'));
   const footer = await ejs.renderFile(path.join(__dirname, '/views/footer.ejs'));
 
