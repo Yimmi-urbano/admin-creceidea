d5362fa0279b13b60c122670e8007df49a8ec29f
yimmisam <111089385+yimmisam@users.noreply.github.com>
2023-07-20 22:10:46 -0500

    add page

--- server.js
+++ server.js
@@ -2,21 +2,37 @@ const express = require('express');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
-app.set('view engine', 'ejs'); 
-app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
-app.get('/',async (req, res) => {
+// Ruta para la página de inicio
+app.get('/', async (req, res) => {
   const html = ReactDOMServer.renderToString(
-    React.createElement('h1', null, '¡Bienvenido a mi app')
+    React.createElement('h1', null, '¡Hola desde el servidor!')
   );
 
-  const header = await ejs.renderFile(__dirname + '/views/header.ejs');
-  const footer = await ejs.renderFile(__dirname + '/views/footer.ejs');
-  
+  const header = await ejs.renderFile(path.join(__dirname, '/views/header.ejs'));
+  const footer = await ejs.renderFile(path.join(__dirname, '/views/footer.ejs'));
 
-  res.render('index.ejs', { header, footer, content: html }); // Renderizar la plantilla principal (index.ejs) con las variables necesarias
+  res.render('index.ejs', { header, footer, content: html });
+});
+
+// Ruta para la página de productos
+app.get('/productos', async (req, res) => {
+  const productos = [
+    { id: 1, nombre: 'Producto 1', precio: 10 },
+    { id: 2, nombre: 'Producto 2', precio: 15 },
+    { id: 3, nombre: 'Producto 3', precio: 20 },
+  ];
+
+  const productList = await ejs.renderFile(path.join(__dirname, '/views/modulos/productList.ejs'), { productos });
+  const header = await ejs.renderFile(path.join(__dirname, '/views/header.ejs'));
+  const footer = await ejs.renderFile(path.join(__dirname, '/views/footer.ejs'));
+
+  res.render('index.ejs', { header, footer, content: productList });
 });
 
 app.use('/client.js', express.static('client.js'));
