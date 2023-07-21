fc95f503ada7b559921eac38709210e7d73c4d80
yimmisam <111089385+yimmisam@users.noreply.github.com>
2023-07-21 03:54:04 -0500

    nuevo 3

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const ejs = require('ejs');
 const path = require('path');
+const cdnStorage = 'https://tiendas.agencsi.com/storage'
 
 
 const app = express();
@@ -10,17 +11,18 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', async (req, res) => {
-  const requestedDomain = req.get('host');
+  const requestedDomain = 'hoppedidos.com' //req.get('host');
   const html = ReactDOMServer.renderToString(
     React.createElement('h1', null, '¡Hola desde el servidor! '+requestedDomain)
   );
   const header = await ejs.renderFile(path.join(__dirname, '/views/header.ejs'));
   const footer = await ejs.renderFile(path.join(__dirname, '/views/footer.ejs'));
 
-  res.render('index.ejs', { header, footer, content: html });
+  res.render('index.ejs', { header, footer, content: html, title: requestedDomain, cdnstorage:cdnStorage});
 });
 
 app.get('/productos', async (req, res) => {
+  const requestedDomain = 'hoppedidos.com' //req.get('host');
   const productos = [
     { id: 1, nombre: 'Producto ABC 1', precio: 10 },
     { id: 2, nombre: 'Producto ABC 2', precio: 15 },
@@ -31,11 +33,11 @@ app.get('/productos', async (req, res) => {
   const header = await ejs.renderFile(path.join(__dirname, '/views/header.ejs'));
   const footer = await ejs.renderFile(path.join(__dirname, '/views/footer.ejs'));
 
-  res.render('index.ejs', { header, footer, content: productList });
+  res.render('index.ejs', { header, footer, content: productList , title: requestedDomain});
 });
 
 app.use('/client.js', express.static('client.js'));
 
-app.listen(3000, () => {
-  console.log('Servidor de renderizado iniciado en http://localhost:3000');
+app.listen(3002, () => {
+  console.log('Servidor de renderizado iniciado en http://localhost:3002');
 });
