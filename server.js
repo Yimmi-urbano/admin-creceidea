ad709dc991d60756fe30953bfef6ff12d25a3581
yimmisam <111089385+yimmisam@users.noreply.github.com>
2023-07-31 22:37:26 -0500

    add page upload changes 8

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-//require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const ejs = require('ejs');
 const path = require('path');
+const fetch = require('isomorphic-fetch');
 const urlcdn =  "https://tiendas.agencsi.com";
 const cdnStorage = "https://tiendas.agencsi.com/storage";
-const requestedDomain = req.get('host');
+const requestedDomain = "hoppedidos.com" //req.get('host');
 
+console.log(requestedDomain)
 
 function generarCodigoAleatorioConTimestamp() {
   const codigoAleatorio = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
@@ -99,31 +101,6 @@ app.get('/', async (req, res) => {
 
 });
 
-
-
-
-/*
-app.get('/catalogo.js', (req, res) => {
-  const filePath = path.join(__dirname, '/views/catalogo.js');
-  res.sendFile(filePath);
-  
-});
-
-app.get('/catalogo/', async (req, res) => {
-  const requestedDomain = 'hoppedidos.com' //req.get('host');
-  const productos = [
-    { id: 1, nombre: 'Producto ABC 1', precio: 10 },
-    { id: 2, nombre: 'Producto ABC 2', precio: 15 },
-    { id: 3, nombre: 'Producto ABC 3', precio: 20 },
-  ];
-
-  const productList = await ejs.renderFile(path.join(__dirname, '/views/modulos/productos/productList.ejs'), { productos });
-  const header = await ejs.renderFile(path.join(__dirname, '/views/header.ejs'));
-  const footer = await ejs.renderFile(path.join(__dirname, '/views/footer.ejs'));
-
-  res.render('catalogo.ejs', { header, footer, content: productList, title: requestedDomain });
-});
-*/
 
 app.listen(3002, () => {
   console.log('Servidor de renderizado iniciado en http://localhost:3002');
